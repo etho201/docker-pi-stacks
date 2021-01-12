b840311e0a03a4bbc059c564e5e8a1e11ad0f0b1
Erik Thomsen <ethomsen86@gmail.com>
2021-01-11 22:52:15 -0500

    Fixed execution point of script

--- dev/wireguard/healthcheck.sh
+++ dev/wireguard/healthcheck.sh
@@ -7,6 +7,8 @@ do
     esac
 done
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 if [[ $(docker run --network=backend --rm curlimages/curl -L -s -o /dev/null -w "%{http_code}" http://wireguard:9091) != "200" ]]
-    then docker-compose up -d && docker run --network=backend --rm curlimages/curl "http://gotify/message?token=${GOTIFY_TOKEN}" -F "title=Transmission" -F "message=Restarted Transmission" -F "priority=5"
+    then cd ${DIR} && docker-compose -f docker-compose.yml up -d && docker run --network=backend --rm curlimages/curl "http://gotify/message?token=${GOTIFY_TOKEN}" -F "title=Transmission" -F "message=Restarted Transmission" -F "priority=5"
 fi
