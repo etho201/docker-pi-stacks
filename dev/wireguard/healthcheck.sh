29927c1f59beebcaa1374d6155e5e32dc525260a
Erik Thomsen <ethomsen86@gmail.com>
2021-01-11 22:15:28 -0500

    Modified container start command

--- dev/wireguard/healthcheck.sh
+++ dev/wireguard/healthcheck.sh
@@ -8,5 +8,5 @@ do
 done
 
 if [[ $(docker run --network=backend --rm curlimages/curl -L -s -o /dev/null -w "%{http_code}" http://wireguard:9091) != "200" ]]
-    then docker restart wireguard transmission qbittorrent && docker run --network=backend --rm curlimages/curl "http://gotify/message?token=${GOTIFY_TOKEN}" -F "title=Transmission" -F "message=Restarted Transmission" -F "priority=5"
+    then docker-compose up -d && docker run --network=backend --rm curlimages/curl "http://gotify/message?token=${GOTIFY_TOKEN}" -F "title=Transmission" -F "message=Restarted Transmission" -F "priority=5"
 fi
