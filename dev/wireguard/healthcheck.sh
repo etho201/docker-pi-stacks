e856a82bf8353b47df761491cb5ae576f843c711
Erik Thomsen <ethomsen86@gmail.com>
2021-01-13 00:49:08 -0500

    Added export path in lieu of absolute paths

--- dev/wireguard/healthcheck.sh
+++ dev/wireguard/healthcheck.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 while getopts t: flag
 do
@@ -9,6 +10,7 @@ done
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-if [[ $(docker run --network=backend --rm curlimages/curl -L -s -o /dev/null -w "%{http_code}" http://wireguard:9091) != "200" ]]
-    then cd ${DIR} && /usr/local/bin/docker-compose -f docker-compose.yml up -d --force-recreate && docker run --network=backend --rm curlimages/curl "http://gotify/message?token=${GOTIFY_TOKEN}" -F "title=Transmission" -F "message=Restarted Transmission" -F "priority=5"
+if [[ $(docker run --network=backend --rm curlimages/curl -L -s -o /dev/null -w "%{http_code}" http://wireguard:9091) != "200" ]]; then
+    docker-compose -f ${DIR}/docker-compose.yml up -d --force-recreate
+    docker run --network=backend --rm curlimages/curl "http://gotify/message?token=${GOTIFY_TOKEN}" -F "title=Transmission" -F "message=Restarted Transmission" -F "priority=5"
 fi
