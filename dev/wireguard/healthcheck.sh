066fcec4cb962b050e9c38a587173599e4eff1d1
Erik Thomsen <ethomsen86@gmail.com>
2021-01-13 00:33:01 -0500

    Added absolute path for docker-compose

--- dev/wireguard/healthcheck.sh
+++ dev/wireguard/healthcheck.sh
@@ -10,5 +10,5 @@ done
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [[ $(docker run --network=backend --rm curlimages/curl -L -s -o /dev/null -w "%{http_code}" http://wireguard:9091) != "200" ]]
-    then cd ${DIR} && docker-compose -f docker-compose.yml up -d --force-recreate && docker run --network=backend --rm curlimages/curl "http://gotify/message?token=${GOTIFY_TOKEN}" -F "title=Transmission" -F "message=Restarted Transmission" -F "priority=5"
+    then cd ${DIR} && /usr/local/bin/docker-compose -f docker-compose.yml up -d --force-recreate && docker run --network=backend --rm curlimages/curl "http://gotify/message?token=${GOTIFY_TOKEN}" -F "title=Transmission" -F "message=Restarted Transmission" -F "priority=5"
 fi
