35a1eb7e41a932e699d710f645cc3fee89c5e885
Erik Thomsen <ethomsen86@gmail.com>
2021-01-13 00:56:06 -0500

    cd into dir of script

--- dev/wireguard/healthcheck.sh
+++ dev/wireguard/healthcheck.sh
@@ -8,9 +8,8 @@ do
     esac
 done
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
 if [[ $(docker run --network=backend --rm curlimages/curl -L -s -o /dev/null -w "%{http_code}" http://wireguard:9091) != "200" ]]; then
-    docker-compose -f ${DIR}/docker-compose.yml up -d --force-recreate
+    cd "$( dirname "${BASH_SOURCE[0]}" )"
+    docker-compose up -d --force-recreate
     docker run --network=backend --rm curlimages/curl "http://gotify/message?token=${GOTIFY_TOKEN}" -F "title=Transmission" -F "message=Restarted Transmission" -F "priority=5"
 fi
