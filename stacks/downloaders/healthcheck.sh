7521d3645e98c0cfecbecb8a602bddff1c6882c4
Erik Thomsen <ethomsen86@gmail.com>
2023-02-13 00:51:25 -0500

    Updated docker network specification

--- stacks/downloaders/healthcheck.sh
+++ stacks/downloaders/healthcheck.sh
@@ -8,8 +8,8 @@ do
     esac
 done
 
-if [[ $(docker run --network=backend --rm curlimages/curl -L -s -o /dev/null -w "%{http_code}" http://wireguard:9091) != "200" ]]; then
+if [[ $(docker run --network=traefik --rm curlimages/curl -L -s -o /dev/null -w "%{http_code}" http://wireguard:9091) != "200" ]]; then
     cd "$( dirname "${BASH_SOURCE[0]}" )"
     docker compose up -d --force-recreate
-    docker run --network=backend --rm curlimages/curl "http://gotify/message?token=${GOTIFY_TOKEN}" -F "title=Transmission" -F "message=Restarted Transmission" -F "priority=5"
+    docker run --network=traefik --rm curlimages/curl "http://gotify/message?token=${GOTIFY_TOKEN}" -F "title=Transmission" -F "message=Restarted Transmission" -F "priority=5"
 fi
