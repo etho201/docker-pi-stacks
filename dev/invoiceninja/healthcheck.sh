733d7bb89b08341df3760f8dccff70d1c67924a8
Erik Thomsen <ethomsen86@gmail.com>
2024-06-25 21:30:28 -0400

    Added external healthcheck script for Invoice Ninja

--- dev/invoiceninja/healthcheck.sh
+++ dev/invoiceninja/healthcheck.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-while getopts t: flag
-do
-    case "${flag}" in
-        t) GOTIFY_TOKEN=${OPTARG};;
+# Parse Command Line Arguments
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -gt*|--gotify-token*)
+            if [[ "$1" != *=* ]]; then shift; fi # Value is next arg if no `=`
+            GOTIFY_TOKEN="${1#*=}"
+            ;;
+        -in*|--invoiceninja-token*)
+            if [[ "$1" != *=* ]]; then shift; fi # Value is next arg if no `=`
+            IN_API_TOKEN="${1#*=}"
+            ;;
+        --help|-h)
+            printf "Options:\n"
+            printf "%-30s%-s\n" "  -gt, --gotify-token" "Specify the application token for Gotify"
+            printf "%-30s%-s\n" "  -in, --invoiceninja-token" "Specify the API token for Invoice Ninja"
+            exit 0
+            ;;
+        *)
+            printf "Error: Invalid argument, run --help for valid options.\n"
+            exit 1
     esac
+    shift
 done
 
-if [[ $(docker run --network=traefik --rm curlimages/curl -L -s -o /dev/null -w "%{http_code}" http://invoiceninja:9000) != "200" ]]; then
+if [[ $(docker run --network=traefik --rm curlimages/curl -Lso /dev/null -w "%{http_code}" -X GET http://invoiceninja-nginx/api/v1/health_check -H "X-API-TOKEN: ${IN_API_TOKEN}") != "200" ]]; then
     cd "$( dirname "${BASH_SOURCE[0]}" )"
-    docker compose up -d --force-recreate
+    docker compose up -d --force-recreate invoiceninja-nginx
     docker run --network=traefik --rm curlimages/curl "http://gotify/message?token=${GOTIFY_TOKEN}" -F "title=Invoice Ninja" -F "message=Restarted Invoice Ninja" -F "priority=5"
 fi
+
