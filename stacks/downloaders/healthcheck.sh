9fcfe5f7f7cfa414223f686db8cf7a0d1eaab1c8
Erik Thomsen <ethomsen86@gmail.com>
2026-09-18 16:41:41 -0400

    Switched to not restart soulseek automatically

--- stacks/downloaders/healthcheck.sh
+++ stacks/downloaders/healthcheck.sh
@@ -15,6 +15,6 @@ if [[ $(docker inspect wireguard -f '{{.State.Running}}' 2>/dev/null) == "false"
     # Recreate from the service's own compose file + env so custom settings
     # (volumes, network_mode, labels, PUID/CONFIG_DIR/EXTHDD_DIR, ...) are honored.
     docker compose -f "$COMPOSE_DIR/compose.yaml" --env-file "$COMPOSE_DIR/.env" \
-        up -d --force-recreate wireguard transmission sabnzbd qbittorrent soulseek nzbget
+        up -d --force-recreate wireguard transmission sabnzbd qbittorrent nzbget
     docker run --network=traefik --rm curlimages/curl "http://gotify/message?token=${GOTIFY_TOKEN}" -F "title=Wireguard" -F "message=Wireguard container found to be unhealthy. Successfully restarted the container!" -F "priority=5"
 fi
